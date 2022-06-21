dc5677a49fb39bb633582223c8c3b5d9eeb94c26
devsanjog <sanjog121@gmail.com>
2022-06-21 08:51:25 +0530

    admin layout setup

---
+++ resources/views/elements/admin/header.blade.php
@@ -0,0 +1,22 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>{{ 'Real Estate Admin' }}</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('css/admin/feather/feather.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin/mdi/css/materialdesignicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin/typicons/typicons.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin/simple-line-icons/css/simple-line-icons.css') }}">
+    <!-- endinject -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('css/admin/vertical-layout-light/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin/main.css') }}">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
---
+++ resources/views/elements/admin/scripts.blade.php
@@ -0,0 +1,13 @@
+<!-- plugins:js -->
+<script src="{{ asset('js/admin/main.js') }}"></script>
+
+<!-- inject:js -->
+<script src="{{ asset('js/admin/hoverable-collapse.js') }}"></script>
+<script src="{{ asset('js/admin/template.js') }}"></script>
+<script src="{{ asset('js/admin/settings.js') }}"></script>
+<script src="{{ asset('js/admin/todolist.js') }}"></script>
+<!-- endinject -->
+<!-- Custom js for this page-->
+<script src="{{ asset('js/admin/jquery.cookie.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/admin/dashboard.js') }}"></script>
+{{--<script src="{{ asset('js/app.js') }}" defer></script>--}}
